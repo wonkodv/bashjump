b90e0d31565f83d214f39e520a184116c8cc2d33
Wonko der Verständige <Wonko@HansTool.org>
2020-05-01 02:39:53 +0200

    check: check that sqlite is created

--- jump.bash
+++ jump.bash
@@ -5,7 +5,8 @@ bashjump_sql(){
 bashjump_cd(){
 	builtin cd "${1:-$HOME}"
 	bashjump_sql "INSERT INTO jumps (dir,prio) VALUES ( '$(bashjump_escape s "$PWD")' , 1 ) ;" &>/dev/null  ||
-	bashjump_sql "UPDATE  jumps SET prio = prio + 1 where dir = '$(bashjump_escape s "$PWD")';" &>/dev/null
+	bashjump_sql "UPDATE  jumps SET prio = prio + 1 where dir = '$(bashjump_escape s "$PWD")';" &>/dev/null ||
+	echo "bashjump has database problems" > &2
 }
 
 bashjump_jump(){
@@ -15,89 +16,88 @@ bashjump_jump(){
 	local d
 	local p
 
-	local opt_results=false
-	local opt_query=false
-	while [ -n "$1" ]
-	do
-		x="$1"
-		shift
-		case $x in
-			-r)
-				opt_results=true
-			;;
-			-q)
-				opt_query=true
-			;;
-			-a)
-				d="$1"
-				if [ -z "$d" ]
-				then
-					d="$PWD"
-				else
-					d=$(cd "$d" ; pwd )
-				fi
-				echo "add $d"
-				bashjump_sql "INSERT INTO jumps (dir,prio) VALUES ( '$(bashjump_escape s "$d")' , 1 ) ;"
-				return
-			;;
-			-P)
-				p="${1:-10}"
-				d="$2"
-				if [ -z "$d" ]
-				then
-					d="$PWD"
-				else
-					d=$(cd "$d" ; pwd )
-				fi
-				echo "set prio of $d to $p"
-				bashjump_sql "UPDATE  jumps SET prio = '$(bashjump_escape s "$p")' where dir = '$(bashjump_escape s "$d")';"
-				return
-			;;
-			-p)
-				p="${1:-10}"
-				d="$2"
-				if [ -z "$d" ]
-				then
-					d="$PWD"
-				else
-					d=$(cd "$d" ; pwd )
-				fi
-
-				echo "increase prio of $d by $p"
-				bashjump_sql "UPDATE  jumps SET prio = prio + '$(bashjump_escape s "$p")' where dir = '$(bashjump_escape s "$d")';"
-				return
-			;;
-			-h)
-				echo "bashJump:
-					j pattern*
-						change to directory which contains all patterns in order
-						which does not have a parent directory that matches
-						and which has the highest priority of all matches
-						placeholders:
-							_ for one character
-							% for 0 or more
-							. to only search in subdirectories of current
-					j -r pattern*
-						print all matches for pattern
-					j -q
-						print final pattern
-					j -a [dir]
-						add dir or pwd to table
-					j -p [dir] [prio]
-						increase priority of dir or pwd by prio or 10
-					j -P [dir] [prio]
-						set priority of pwd or dir to prio or 10
-				" | sed 's/^\t\t\t\t//'
-				return
-			;;
-			.)
-				q="$(bashjump_escape l "$PWD")${q}"
-			;;
-			*)
-				q="${q}$(bashjump_escape l "$x")%"
-			;;
-		esac
-	done
+    local opt_results=false
+    local opt_query=false
+    while [ -n "$1" ]
+    do
+        x="$1"
+        shift
+        case $x in
+            -r)
+                opt_results=true
+                ;;
+            -q)
+                opt_query=true
+                ;;
+            -a)
+                d="$1"
+                if [ -z "$d" ]
+                then
+                    d="$PWD"
+                else
+                    d=$(cd "$d" ; pwd )
+                fi
+                echo "add $d"
+                bashjump_sql "INSERT INTO jumps (dir,prio) VALUES ( '$(bashjump_escape s "$d")' , 1 ) ;"
+                return
+                ;;
+            -P)
+                p="${1:-10}"
+                d="$2"
+                if [ -z "$d" ]
+                then
+                    d="$PWD"
+                else
+                    d=$(cd "$d" ; pwd )
+                fi
+                echo "set prio of $d to $p"
+                bashjump_sql "UPDATE  jumps SET prio = '$(bashjump_escape s "$p")' where dir = '$(bashjump_escape s "$d")';"
+                return
+                ;;
+            -p)
+                p="${1:-10}"
+                d="$2"
+                if [ -z "$d" ]
+                then
+                    d="$PWD"
+                else
+                    d=$(cd "$d" ; pwd )
+                fi
+                echo "increase prio of $d by $p"
+                bashjump_sql "UPDATE  jumps SET prio = prio + '$(bashjump_escape s "$p")' where dir = '$(bashjump_escape s "$d")';"
+                return
+                ;;
+            -h)
+                echo "bashJump:
+                j pattern*
+                change to directory which contains all patterns in order
+                which does not have a parent directory that matches
+                and which has the highest priority of all matches
+                placeholders:
+                _ for one character
+                % for 0 or more
+                . to only search in subdirectories of current
+                j -r pattern*
+                print all matches for pattern
+                j -q
+                print final pattern
+                j -a [dir]
+                add dir or pwd to table
+                j -p [dir] [prio]
+                increase priority of dir or pwd by prio or 10
+                j -P [dir] [prio]
+                set priority of pwd or dir to prio or 10
+                " | sed 's/^\t\t\t\t//'
+                return
+                ;;
+            .)
+                q="$(bashjump_escape l "$PWD")${q}"
+                ;;
+            *)
+                q="${q}$(bashjump_escape l "$x")%"
+                ;;
+        esac
+    done
 
 	sql_from_where_order="
 		FROM
@@ -154,9 +154,9 @@ bashjump_escape()
 
 
 bashjump_check_or_create_db(){
-if [ ! -f "$BASHJUMP_HISTORY" ]
+if ! bashjump_sql "SELECT 1 FROM jumps;" &> /dev/null
 then
-	if ! sqlite3 "$BASHJUMP_HISTORY" "CREATE TABLE jumps(
+	if ! bashjump_sql "CREATE TABLE jumps(
 		dir STRING PRIMARY KEY NOT NULL,
 		prio INT NOT NULL
 	);"
