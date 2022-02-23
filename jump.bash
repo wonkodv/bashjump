b1f298cf078881e722c3366003c9b146f4358bfb
Wonko der Verständige <Wonko@HansTool.org>
2022-02-24 00:43:35 +0100

    tabs

--- jump.bash
+++ jump.bash
@@ -1,20 +1,20 @@
 bashjump_sql(){
-	sqlite3 "$BASHJUMP_HISTORY" "$1"
+    sqlite3 "$BASHJUMP_HISTORY" "$1"
 }
 
 bashjump_cd(){
-	builtin cd "${1:-$HOME}"
-	bashjump_sql "INSERT INTO jumps (dir,prio) VALUES ( '$(bashjump_escape s "$PWD")' , 1 ) ;" &>/dev/null  ||
-	bashjump_sql "UPDATE  jumps SET prio = prio + 1 where dir = '$(bashjump_escape s "$PWD")';" &>/dev/null ||
-	echo "bashjump has database problems" >&2
+    builtin cd "${1:-$HOME}"
+    bashjump_sql "INSERT INTO jumps (dir,prio) VALUES ( '$(bashjump_escape s "$PWD")' , 1 ) ;" &>/dev/null  ||
+    bashjump_sql "UPDATE  jumps SET prio = prio + 1 where dir = '$(bashjump_escape s "$PWD")';" &>/dev/null ||
+    echo "bashjump has database problems" >&2
 }
 
 bashjump_jump(){
 
-	local q="%"
-	local x
-	local d
-	local p
+    local q="%"
+    local x
+    local d
+    local p
 
     local opt_results=false
     local opt_query=false
@@ -99,70 +99,70 @@ bashjump_jump(){
         esac
     done
 
-	sql_from_where_order="
-		FROM
-			jumps j1
-		WHERE
-				j1.dir LIKE '$q'
-			AND
-				NOT EXISTS
-				(
-					SELECT
-						1
-					FROM
-						jumps j2
-					WHERE
-							j1.dir != j2.dir
-						AND
-							j2.dir LIKE '$q'
-						AND
-							1 = INSTR(j1.dir,j2.dir)
-				)
-		ORDER BY prio DESC
-		"
+    sql_from_where_order="
+        FROM
+            jumps j1
+        WHERE
+                j1.dir LIKE '$q'
+            AND
+                NOT EXISTS
+                (
+                    SELECT
+                        1
+                    FROM
+                        jumps j2
+                    WHERE
+                            j1.dir != j2.dir
+                        AND
+                            j2.dir LIKE '$q'
+                        AND
+                            1 = INSTR(j1.dir,j2.dir)
+                )
+        ORDER BY prio DESC
+        "
 
-	$opt_query && echo "query: $q" && return
-	$opt_results && sqlite3 -separator "	" "$BASHJUMP_HISTORY" "SELECT prio,dir $sql_from_where_order" && return
-	d="`bashjump_sql "SELECT dir $sql_from_where_order LIMIT 1"`"
-	if [ -d "$d" ]
-	then
-		builtin cd "$d"
-	elif [ -z "$d" ]
-	then
-		echo "no dir for query $q"
-	else
-		read -e -p "$d seems deleted, remove from history? [Y/n] " r
-		if [[ "$r" != n && "$r" != "N" ]]
-		then
-			bashjump_sql "DELETE FROM  jumps WHERE dir LIKE '$(bashjump_escape l "$d")%'  ESCAPE '\\';"
-		fi
-	fi
+    $opt_query && echo "query: $q" && return
+    $opt_results && sqlite3 -separator "	" "$BASHJUMP_HISTORY" "SELECT prio,dir $sql_from_where_order" && return
+    d="`bashjump_sql "SELECT dir $sql_from_where_order LIMIT 1"`"
+    if [ -d "$d" ]
+    then
+        builtin cd "$d"
+    elif [ -z "$d" ]
+    then
+        echo "no dir for query $q"
+    else
+        read -e -p "$d seems deleted, remove from history? [Y/n] " r
+        if [[ "$r" != n && "$r" != "N" ]]
+        then
+            bashjump_sql "DELETE FROM  jumps WHERE dir LIKE '$(bashjump_escape l "$d")%'  ESCAPE '\\';"
+        fi
+    fi
 }
 
 bashjump_escape()
 {
-	local a
-	a="$2"
-	a="${a//\'/\'\'}"
-	if [[ "$1" == "l" ]]
-	then
-		a="${a//\\/\\\\}"
-		a="${a//%/\\%}"
-	fi
-	echo "$a"
+    local a
+    a="$2"
+    a="${a//\'/\'\'}"
+    if [[ "$1" == "l" ]]
+    then
+        a="${a//\\/\\\\}"
+        a="${a//%/\\%}"
+    fi
+    echo "$a"
 }
 
 
 bashjump_check_or_create_db(){
 if ! bashjump_sql "SELECT 1 FROM jumps;" &> /dev/null
 then
-	if ! bashjump_sql "CREATE TABLE jumps(
-		dir STRING PRIMARY KEY NOT NULL,
-		prio INT NOT NULL
-	);"
-	then
-		echo "problems"
-		return 1
-	fi
+    if ! bashjump_sql "CREATE TABLE jumps(
+        dir STRING PRIMARY KEY NOT NULL,
+        prio INT NOT NULL
+    );"
+    then
+        echo "problems"
+        return 1
+    fi
 fi
 }
